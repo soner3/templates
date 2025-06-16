1e46bed663df247c2fd74c5b2e192a8a6b47de08
soner3 <sonerastan@icloud.com>
2025-06-16 18:21:08 +0200

    init: Add initial project structure and configuration for JWT authentication

---
+++ auth-server-templates/jwt-auth/java/jwt-auth/src/test/java/com/sonastan/jwt_auth/JwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonastan.jwt_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
