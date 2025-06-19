0d3e65ff00a8948b78315f9b6b0fdccc1270603b
soner3 <sonerastan@icloud.com>
2025-06-19 07:12:50 +0200

    feat: Add ApiControllerAdvice for centralized exception handling and validation error responses

---
+++ auth-server-templates/jwt-auth/java/jwt-auth/src/main/java/com/sonastan/jwt_auth/interfaces/rest/controller/ApiControllerAdvice.java
@@ -0,0 +1,42 @@
+package com.sonastan.jwt_auth.interfaces.rest.controller;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+    private ResponseEntity<ProblemDetail> createProblemDetail(String title, HttpStatus status, Exception ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        String type = ex.getClass().getName().replace('.', '/');
+        problemDetail.setTitle(title);
+        problemDetail.setDetail(ex.getMessage());
+        problemDetail.setType(URI.create(type));
+        problemDetail.setStatus(status.value());
+        return ResponseEntity.status(status).body(problemDetail);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ProblemDetail problemDetail = createProblemDetail("Validation Error", HttpStatus.BAD_REQUEST, ex).getBody();
+        Map<String, Object> validationErrors = ex
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        err -> ((FieldError) err).getField(),
+                        err -> err.getDefaultMessage()));
+        problemDetail.setProperties(validationErrors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+    }
+
+}
